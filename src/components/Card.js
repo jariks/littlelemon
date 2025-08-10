d651bec05c112139e68d5128a65941fae51628c3
jariks <jorislovas@gmail.com>
2025-08-11 00:55:15 +0300

    Finished Reserve table section!

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
 import { H1, H2, H3, P } from "./Headings"
 const Card = ({ source, name, price, content }) => {
   return (
-    <div className="max-w-xs">
-      <img className="rounded-t-[1rem] w-full" src={source} />
-      <div>
+    <div className="w-full sm:max-w-xs flex flex-col overflow-hidden">
+      <img className="overflow-hidden w-full aspect-[4/3] rounded-t-[1rem] object-cover h-full" src={source} />
+      <div className="bg-gray-200 flex flex-col flex-1 rounded-b-[1rem]">
         <div className="py-4 px-6 flex justify-between items-center">
-          <H3>Greek Salad</H3>
-          <h3 className="font-sans font-semibold text-xl sm:text-2xl leading-snug text-red-400">$19.99</h3>
+          <h3 className="font-semibold leading-snug text-lg sm:text-xl font-sans">{name}</h3>
+          <h3 className="font-sans font-semibold text-lg sm:text-xl leading-snug text-red-400">${price}</h3>
         </div>
-        <P additional="px-6">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</P>
-        <button className="rounded-full bg-lemon-green text-white hover:bg-lemon-yellow hover:text-black px-4 py-6">Order delivery</button>
+        <P additional="px-6 flex-grow">{content}</P>
+        <button className="box-border w-[calc(100%-1rem)] rounded-full bg-lemon-green text-white hover:bg-lemon-yellow hover:text-black p-2 mx-2 mb-4">Order delivery</button>
       </div>
 
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -26,14 +26,14 @@ function Main() {
 
       <section className="w-full">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-          <div className="w-full flex gap-4 justify-between">
+          <div className="w-full flex gap-4 justify-between items-center mb-4">
             <H2>Reserve a table</H2>
             <ButtonPrimary> online menu</ButtonPrimary>
           </div>
-          <div className="flex justify-evenly gap-4">
-            <Card source={card1} />
-            <Card source={card1} />
-            <Card source={card1} />
+          <div className="grid grid-rows-3 grid-cols-1 sm:grid-rows-1 sm:grid-cols-3 gap-6 items-stretch">
+            <Card source={card1} name="Greek Salad" price="12.99" content="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons."/>
+            <Card source={card2} name="Bruchetta" price="5.99" content="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons."/>
+            <Card source={card3} name="Lemon Dessert" price="8.99" content="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons."/>
           </div>
         </div>
       </section>
